f6e976025a72fdb737459a18f2d3843dd50f8a11
phcodesage <rechceltoledo@gmail.com>
2026-03-28 05:56:13 +0800

    fix: add .next/ to .gitignore and untrack build artifacts

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Sh8NYc0RLQQdlW1It6w23M4YIsniY9uddoNWzl6dhoM=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_0mf9_3o._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0.yx0dl._.js","static/chunks/app_page_tsx_0~3a212._.js","static/chunks/node_modules_lenis_dist_lenis_0tuy2nf.css"],
-    source: "dynamic"
-});
